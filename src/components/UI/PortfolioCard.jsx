257e9f437f223ef608e379fe9afaed6926cd7eaa
Alan Lee <aflee@wisc.edu>
2024-02-12 11:12:44 -0600

    Finishing touched for MVP

--- src/components/UI/PortfolioCard.jsx
+++ src/components/UI/PortfolioCard.jsx
@@ -3,10 +3,10 @@ import KeywordList from './KeywordList.jsx';
 function PortfolioCard (props) {
 
     return ( 
-        <div className = "card" id = {`project-${props.id}`}>
-            <h2>{props.name}</h2>
+        <div className = "card m-2" id = {`project-${props.id}`}>
+            <h2 className = "text-center">{props.name}</h2>
             <img src = {props.img}></img>
-            <h6>Key-Words</h6>
+            <h5 className = "ps-4 mt-4 mb-0 text-center">Key-Words</h5>
             <KeywordList list = {props.keywords} />
             <p>{props.description}</p>
             <h4><a href = {props.deployed}>Deployed Website</a></h4>
